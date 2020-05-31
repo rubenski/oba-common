6e20f7fb5c995dc142e72f9992d16833f5ce0b7e
Ruben <rubenski@gmail.com>
2020-05-31 17:11:07 +0200

    RequestContext is too generic, it is for application's calling the API

--- src/main/java/com/obaccelerator/common/ObaConstant.java
+++ src/main/java/com/obaccelerator/common/ObaConstant.java
@@ -2,10 +2,14 @@ package com.obaccelerator.common;
 
 public class ObaConstant {
 
-    public static final String INTERNAL_TOKEN_HEADER = "internal-token";
     public static final String ROLE_CLAIM = "role";
     public static final String APPLICATION_ID_CLAIM = "application-id";
-    public static final String HEADER_AUTHORIZATION = "authorization";
-    public static final String API_CLIENT_ROLE_NAME = "api-client";
+
+
+    public static final String ROLE_APPLICATION = "application";
+    public static final String ROLE_PORTAL_ADMIN = "portal_admin";
+    public static final String ROLE_PORTAL_ORGANIZATION = "portal_organization";
+    public static final String ROLE_OBA_ANONYMOUS = "abo_anonymous";
+
 
 }
